a90d82db4518e8de6c9e7a78e62d1eeedcd21664
nxf24159 <linh.nguyen.hoang@nxp.com>
2018-02-13 15:36:31 +0700

    remove absolute path

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
-f = open("I:\Documents\\ML\\transcript_viqr.txt",'r')
+import os
+f = open(os.path.dirname(os.path.abspath(__file__))+"\\transcript_viqr.txt",'r')
 s = f.readline()
 phuam = ['ngh','qu','ch','gh','kh','ng','nh','ph','th','tr','gi','dd','b','c','d','g','h','k','l','m','n','p','q','r','s','t','v','x']
 nguyenam = ['u+','o^','o+','e^','a(','a^','a','e','i','o','u','y']
 dau = ['`',"'",'.','~','?']
 print(dau)
+
