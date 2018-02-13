b5dec854fcc057a4c1ae74bb7c4817b20ec62622
limited <limited>
2018-02-13 12:01:39 +0700

    Almost nothing

---
+++ batch_rename.py
@@ -0,0 +1,9 @@
+import os
+path = "i:\\documents\\ml"
+for fname in os.listdir(path):
+	if fname[0] == 's':
+		num = int(fname[1:len(fname)-4])
+		snum = fname[1:len(fname)-4]
+		snum2 = snum.zfill(4)
+		os.rename(os.path.join(path,fname),os.path.join(path,'s'+snum2+'.wav'))
+
