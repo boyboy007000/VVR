cb3c2077ec7893972e578b506fb466121ca8e143
limited <limited>
2018-02-24 02:05:35 +0700

    complete fix 3 sec for sample, white noise

--- fix_sample_3_sec.py
+++ fix_sample_3_sec.py
@@ -1,14 +1,31 @@
 import wave
 import struct
-import pywt
-import matplotlib.pyplot as plt
+import random
 import os
 import sys
 
+numframe3s = 24000
+scale = 0.997
+minrnd = round(-32768*(1-scale)) + 1
+maxrnd = round(32767 *(1-scale)) - 1
+
 fl = os.listdir(os.path.dirname(os.path.abspath(__file__)))
 for f in fl:
-    if f.startswith('s'):
+    if f.startswith('s') and f.endswith('wav'):
         w = wave.open(os.path.dirname(os.path.abspath(__file__))+ "\\" + f,"rb")
         w.rewind();
-        print(w.getnframes())
-        w.close()
+        if w.getnframes() > numframe3s:
+            print(f + " more than 3s")
+        elif w.getnframes() < numframe3s:
+            ww = wave.open(os.path.dirname(os.path.abspath(__file__))+ "\\x" + f[1:],"wb")
+            pr = w.getparams()
+            ons = pr.nframes;
+            pr = pr._replace(nframes=numframe3s)
+            ww.setparams(pr)
+            for i in range(0,ons):
+                ww.writeframesraw(struct.pack("<h",round(struct.unpack("<h",w.readframes(1))[0]*scale)+random.randint(minrnd,maxrnd)))
+            for i in range(ons,numframe3s):
+                ww.writeframesraw(struct.pack("<h",random.randint(minrnd,maxrnd)))
+            ww.close()
+        w.close()            
+            
--- wavelet_gen.py
+++ wavelet_gen.py
@@ -4,8 +4,14 @@ import pywt
 import matplotlib.pyplot as plt
 import os
 
-w = wave.open(os.path.dirname(os.path.abspath(__file__))+ "\\voice__.wav","rb")
-w.rewind();
+w = wave.open(os.path.dirname(os.path.abspath(__file__))+ "\\s0001.wav","rb")
+w.rewind()
+p = w.getparams()
+print(p)
+p = p._replace(nframes=20)
+print(p)
+
+exit()
 data = []
 for i in range(0,w.getnframes()):
     data.append(struct.unpack("<h",w.readframes(1))[0])
